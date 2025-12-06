56423edeb782db1e8d8461db2bf88f3b6a3a2ca3
Abhinav Kumar Singh <87404569+abhinav29102005@users.noreply.github.com>
2025-12-06 13:07:59 +0530

    practice 3

--- practice/mergesort.cpp
+++ practice/mergesort.cpp
@@ -4,28 +4,53 @@ using namespace std;
 void mergeArrays(int arr[], int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
-    int a[n1], b[n2];
-    for(int i = 0; i < n1; i++) a[i] = arr[l + i];
-    for(int i = 0; i < n2; i++) b[i] = arr[m + 1 + i];
+
+    int a[n1], b[n2];  // Works in g++ as an extension; for strict C++ use vector<int>
+
+    for (int i = 0; i < n1; i++) a[i] = arr[l + i];
+    for (int i = 0; i < n2; i++) b[i] = arr[m + 1 + i];
+
     int i = 0, j = 0, k = l;
-    while(i < n1 && j < n2) {
-        if(a[i] <= b[j]) arr[k++] = a[i++];
+    while (i < n1 && j < n2) {
+        if (a[i] <= b[j]) arr[k++] = a[i++];
         else arr[k++] = b[j++];
     }
-    while(i < n1) arr[k++] = a[i++];
-    while(j < n2) arr[k++] = b[j++];
+    while (i < n1) arr[k++] = a[i++];
+    while (j < n2) arr[k++] = b[j++];
 }
+
 void mergeSort(int arr[], int l, int r) {
-    if(l < r) {
+    if (l < r) {
         int m = l + (r - l) / 2;
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
         mergeArrays(arr, l, m, r);
     }
 }
+
 int main() {
-    int arr[] = {5, 2, 8, 1, 3};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];   // max 100 elements
+    if (n > 100) {
+        cout << "Max allowed is 100\n";
+        return 0;
+    }
+
+    cout << "Enter " << n << " integers:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
     mergeSort(arr, 0, n - 1);
-    for(int i = 0; i < n; i++) cout << arr[i] << " ";
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
