0682f89ab95bada14231118ec67cc6691ad5afbd
Abhinav Kumar Singh <87404569+abhinav29102005@users.noreply.github.com>
2025-12-06 15:30:31 +0530

    practice 4

---
+++ practice/evalques.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+struct Node {
+    char key[50];    
+    Node *left;
+    Node *right;
+};
+Node* createnode(const char s[]) {
+    Node* node = new Node;
+    strcpy(node->key, s);
+    node->left = node->right = NULL;
+    return node;
+}
+void insert(Node* &root, const char s[]) {
+    if (root == NULL) {
+        root = createnode(s);
+        return;
+    }
+    Node* cur = root;
+    while (true) {
+        int cmp = strcmp(s, cur->key);
+
+        if (cmp < 0) {                      
+            if (cur->left == NULL) {
+                cur->left = createnode(s);
+                break;
+            } else {
+                cur = cur->left;
+            }
+        } else if (cmp > 0) {              
+            if (cur->right == NULL) {
+                cur->right = createnode(s);
+                break;
+            } else {
+                cur = cur->right;
+            }
+        } else {
+            break;
+        }
+    }
+}
+Node* findSibling(Node* root, const char key[]) {
+    Node* parent = NULL;
+    Node* cur = root;
+    while (cur != NULL) {
+        int cmp = strcmp(key, cur->key);
+        if (cmp == 0) {
+            break;           
+        }
+        parent = cur;
+        if (cmp < 0)
+            cur = cur->left;
+        else
+            cur = cur->right;
+    }
+    if (cur == NULL || parent == NULL) {
+        return NULL;
+    }
+    if (parent->left == cur)
+        return parent->right;
+    else
+        return parent->left;
+}
+
+int main() {
+    Node* root = NULL;
+    int n;
+    cout << "Enter number of strings: ";
+    cin >> n;
+    cout << "Enter " << n << " strings:\n";
+    for (int i = 0; i < n; ++i) {
+        char buf[50];
+        cin >> buf;
+        insert(root, buf);
+    }
+    char query[50];
+    cout << "Enter key to find its sibling: ";
+    cin >> query;
+    Node* sib = findSibling(root, query);
+    if (sib == NULL) {
+        cout << "No sibling found.\n";
+    } else {
+        cout << "Sibling of \"" << query << "\" is: " << sib->key << "\n";
+    }
+    return 0;
+}
--- practice/quicksort.cpp
+++ practice/quicksort.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int partitionArray(int arr[], int low, int high) {
+int arrketokde(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
     for(int j = low; j < high; j++) {
@@ -12,16 +12,16 @@ int partitionArray(int arr[], int low, int high) {
     swap(arr[i + 1], arr[high]);
     return i + 1;
 }
-void quickSort(int arr[], int low, int high) {
+void quicksort(int arr[], int low, int high) {
     if(low < high) {
-        int pi = partitionArray(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pi = arrketokde(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
     }
 }
 int main() {
     int arr[] = {7, 3, 1, 9, 4};
     int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n - 1);
+    quicksort(arr, 0, n - 1);
     for(int i = 0; i < n; i++) cout << arr[i] << " ";
 }
